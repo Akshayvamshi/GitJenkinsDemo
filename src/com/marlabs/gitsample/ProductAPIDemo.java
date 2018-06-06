516925e7b2b8f43ae7af7ff1c6a5bd12c8c4b87f
Akshayvamshi <agr78@mail.umkc.edu>
2018-06-05 21:33:14 -0400

    initial files

---
+++ src/com/marlabs/gitsample/ProductAPIDemo.java
@@ -0,0 +1,11 @@
+package com.marlabs.gitsample;
+
+public class ProductAPIDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ProductAPIExample product = new ProductAPIExample(1,"book",20.0d);
+		System.out.println(product);
+	}
+
+}
