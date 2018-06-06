6ea9d792534d12a6fd3c5d5046c40b74d953b623
Akshayvamshi <agr78@mail.umkc.edu>
2018-06-05 21:56:09 -0400

    Added print for product details on ProductAPIDemo

--- src/com/marlabs/gitsample/ProductAPIDemo.java
+++ src/com/marlabs/gitsample/ProductAPIDemo.java
@@ -5,6 +5,7 @@ public class ProductAPIDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ProductAPIExample product = new ProductAPIExample(1,"book",20.0d);
+		System.out.println("******ProductDetails*******");
 		System.out.println(product);
 	}
 
